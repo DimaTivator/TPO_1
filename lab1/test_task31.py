8114ee2f68455c1d68851c2bda35a263ae8b39ec
dariavasina <72202323+dariavasina@users.noreply.github.com>
2025-02-27 23:26:29 +0300

    52

--- lab1/test_task31.py
+++ lab1/test_task31.py
@@ -1,7 +1,6 @@
 import pytest
 
-from test_task3 import *
-
+from task3 import *
 
 @pytest.fixture
 def human():
@@ -11,7 +10,6 @@ def human():
     return h
 
 def test_express_contempt(human):
-    """Проверка выражения презрения: пауза, интонация, тембр."""
     human.express_contempt()
     assert "презрение" in [e.name for e in human.emotions]
     assert human.speech.pause.duration == 1.0
@@ -19,7 +17,6 @@ def test_express_contempt(human):
     assert human.speech.timbre.quality == 3
 
 def test_express_terror(human):
-    """Проверка выражения ужаса: пауза, интонация, тембр."""
     human.express_terror()
     assert "ужас" in [e.name for e in human.emotions]
     assert human.speech.pause.duration == 0.1
@@ -27,39 +24,33 @@ def test_express_terror(human):
     assert human.speech.timbre.quality == 1
 
 def test_invalid_emotion_intensity():
-    """Ошибка при недопустимой интенсивности эмоции."""
     with pytest.raises(ValueError):
         Emotion("гнев", 11)
     with pytest.raises(ValueError):
         Emotion("страх", -1)
 
 def test_get_emotion_intensity(human):
-    """Получение интенсивности эмоции."""
     human.express_contempt()
     assert human.get_emotion_intensity("презрение") == 10
     assert human.get_emotion_intensity("радость") == 0
 
 def test_invalid_timbre_quality():
-    """Проверка недопустимых значений для качества тембра."""
     with pytest.raises(ValueError):
-        Timbre(4)  # Значение больше 3
+        Timbre(4)
     with pytest.raises(ValueError):
-        Timbre(-1)  # Значение меньше 0
+        Timbre(-1)
 
 def test_modify_pause(human):
-    """Проверка изменения паузы в речи."""
     new_pause = Pause(2.0)
     human.speech.modify_pause(new_pause)
     assert human.speech.pause.duration == 2.0
 
 def test_modify_intonation(human):
-    """Проверка изменения интонации в речи."""
     new_intonation = Intonation(6)
     human.speech.modify_intonation(new_intonation)
     assert human.speech.intonation.level == 6
 
 def test_modify_timbre(human):
-    """Проверка изменения тембра в речи."""
     new_timbre = Timbre(1)
     human.speech.modify_timbre(new_timbre)
     assert human.speech.timbre.quality == 1
